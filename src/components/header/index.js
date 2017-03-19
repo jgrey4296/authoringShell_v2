557e5ec7050b9a30dc5b71f6876ea235a8f4f630
John Grey <jgrey@ucsc.edu>
2017-03-19 17:37:31 +0000

    added path rendering to the header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,10 +3,14 @@ import { Link } from 'preact-router';
 import style from './style';
 
 export default class Header extends Component {
-	render() {
+	render({ path }) {
+        let pathString = path.map(([name,id])=>{
+            return <span>{name} <span class={style.nodeId}>({id}) </span>/ </span>
+        });
 		return (
 			<header class={style.header}>
-				<h1>JGShell</h1>
+				<h1>JGShell:</h1>
+                <h2>{pathString}</h2>
 			</header>
 		);
 	}
