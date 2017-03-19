62fb9cd48b0a61c797ded276008bc9e190c51f93
John Grey <jgrey@ucsc.edu>
2017-03-19 08:44:52 +0000

    handled input in a react style

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,10 +3,24 @@ import { Link } from 'preact-router';
 import style from './style';
 
 export default class Footer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.props.callback(event.target.value);
+        this.setState({value: ''});
+        
+    }
+
 	render() {
 		return (
 			<footer class={style.footer}>
-                <input id="shellInput" type="text"/>
+                <input id="shellInput" type="text" value={this.state.value} onChange={this.handleChange} />
 			</footer>
 		);
 	}
