a7fd5e93464d4e33517cc1721c8b450c7cc0ccba
John Grey <jgrey@ucsc.edu>
2017-03-19 08:45:11 +0000

    added initial code to render the basic shell

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,45 +1,48 @@
 import { h, Component } from 'preact';
 import style from './style';
-
 import Test from '../test';
-import {value as testlib} from 'testlib';
-
+import Column from '../column';
+import Node from '../node';
+import Footer from '../footer';
+import { Shell } from 'JGShell';
 
 
 export default class Home extends Component {
 
     constructor(){
         super();
-        console.log('Creating home');
-        this.blah = testlib;
-        console.log(this.blah);
+        this.shell = new Shell();
         this.state = {
-            //have the shell object here
-            value1: 'awef',
-            value2: 'awefgg',
-            //Add Data for Inputs, Focus, Right
+            parents : [],
+            children : [],
+            focusNode : this.shell.cwd()
         };
     }
 
-    click(val){
-        var newState = {}
-        if (this.state[val] !== "blah"){
-            newState[val] = "blah";
-        }else{
-            newState[val] = "bloo";
-        }
-        this.setState(newState);
+    componentWillUnmount(){
+        //clean up the shell
     }
 
     //function here to be passed to footer.input for parsing and triggering shell changes 
-
+    parseCallback(text){
+        console.log("Received: " + text);
+        this.shell.parse(text);
+        let result = this.shell.parse('cwd');
+        this.setState({ parents: result.inputs.map((d)=>this.shell.get(d.source.id)) });
+        this.setState({ children: result.outputs.map((d)=>this.shell.get(d.dest.id)) });
+        this.setState({ focusNode: result.node });
+    }
+    
     //functions here to be passed to the shell to trigger updates of Inputs,Focus,Right
     
 	render() {
+        //todo: add a focus node column
 		return (
 			    <div class={style.home}>
-                <Test pos="5%" side="left" val={this.state.value1} clicked={()=> this.click('value1')}/>
-                <Test pos="5%" side="right"val={this.state.value2} clicked={()=> this.click('value2')}/>
+                <Column pos="5%" side="left" data={this.state.parents} name="Parents"/>
+                <Node data={this.state.focusNode} />
+                <Column pos="5%" side="right" data={this.state.children} name="Children"/>
+                <Footer callback={(text) => { this.parseCallback(text) }}/>
                 </div>
 		);
 	}
