7f8068aa20842a18cee9cc62cbe9d82e0d04ce58
John Grey <jgrey@ucsc.edu>
2017-03-19 12:08:45 +0000

    added the initial node entry component

---
+++ src/components/nodeEntry/index.js
@@ -0,0 +1,33 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class NodeEntry extends Component {
+    constructor(props){
+        super(props);
+        //modify with this.setState
+        this.state = {
+            
+        };
+
+        //if necessary, bind methods
+        //this.aFunc = this.aFunc.bind(this)
+    }
+
+    //Don't forget to destructure:
+    render({ varName, value }, state){
+        let theString = '';
+        if (value !== null){
+            theString = `${varName} : ${value}`;
+        }else{
+            theString = `${varName}`;
+        }
+        
+        return (
+                <li class={style.nodeEntry}>
+                {theString}
+            </li>
+        );
+    }
+
+
+}
