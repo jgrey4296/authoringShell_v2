6a5cc03da549f350a82e1297fc0763e8c32958a4
John Grey <jgrey@ucsc.edu>
2017-03-20 10:29:00 +0000

    moved the shell root into app instead of home

--- src/components/app.js
+++ src/components/app.js
@@ -1,38 +1,70 @@
 import { h, Component } from 'preact';
-import { Router } from 'preact-router';
 
 import Header from './header';
 import Home from './home';
-import Profile from './profile';
+import Footer from './footer';
+import { Shell } from 'JGShell';
 
 export default class App extends Component {
 	/** Gets fired when the route changes.
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
     constructor(){
         super();
+        this.shell = new Shell();
         this.state = {
+            parents : [],
+            children : [],
+            focusNode : this.shell.cwd(),
+            searchState : 'default',
+            searchResults : [],
             path: []
         };
-
-        this.updatePath = this.updatePath.bind(this);
     }
 
-    updatePath(pathArr) {
-        this.setState({ path: pathArr });
+
+    //function here to be passed to footer.input for parsing and triggering shell changes
+    parseCallback(text){
+        let parseResult = this.shell.parse(text);
+        if (parseResult !== null){
+            switch (parseResult.description) {
+                case 'help':
+				    console.log('todo: help');
+                    break;
+                case 'json': {
+                    let text = parseResult.text;
+                    //From: http://stackoverflow.com/questions/10472927/add-content-to-a-new-open-window
+                    window.open('data:application/json;' + (window.btoa?'base64,'+btoa(text):text));
+                }
+                    break;
+                default:
+                    console.log('unrecognised result: ', parseResult);
+            }
+        }
+        let result = this.shell.parse('cwd');
+        console.log('Result:',result);
+        this.setState({ parents: result.inputs.map((d)=>this.shell.get(d.source.id)) });
+        this.setState({ children: result.outputs.map((d)=>this.shell.get(d.dest.id)) });
+        this.setState({ focusNode: result.node });
+        if (result.searchResults.length === 0){
+            if (this.state.searchState !== 'default'){
+                this.setState({ searchState : "closed" });
+            }
+        } else if (this.state.searchState !== 'open'){
+            this.setState({ searchState: "open" });
+        }
+        this.setState({ searchResults : result.searchResults.map((d)=>this.shell.get(d))});
+        this.setState({ path : result.currentPath });
     }
-    
-	render() {
-		return (
-			<div id="app">
-				<Header path={this.state.path} />
-                <Home pathUpdate={this.updatePath} />
-			</div>
-		);
-	}
+
+    render() {
+        return (
+			    <div id="app">
+			    <Header path={this.state.path} />
+                <Home focusNode={this.state.focusNode} parents={this.state.parents} children={this.state.children} searchState={this.state.searchState} searchResults={this.state.searchResults}/>
+                <Footer callback={(text) => { this.parseCallback(text); }} />
+			    </div>
+        );
+    }
 }
