8122ab42c85436e93c3addf60e1c24007685b4b6
John Grey <jgrey@ucsc.edu>
2017-03-20 10:32:08 +0000

    added new stub files for expected components

---
+++ src/components/binding/index.js
@@ -0,0 +1,26 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Binding extends Component {
+    constructor(props){
+        super(props);
+        //modify with this.setState
+        this.state = {
+            
+        };
+
+        //if necessary, bind methods
+        //this.aFunc = this.aFunc.bind(this)
+    }
+
+    //Don't forget to destructure:
+    render(props, state){
+        return (
+                <div>
+
+            </div>
+        );
+    }
+
+
+}
---
+++ src/components/condition/index.js
@@ -0,0 +1,27 @@
+import { h, Component } from 'preact';
+import style from './style';
+
+export default class Condition extends Component {
+    constructor(props){
+        super(props);
+        //modify with this.setState
+        this.state = {
+            
+        };
+
+        //if necessary, bind methods
+        //this.aFunc = this.aFunc.bind(this)
+    }
+
+    //Don't forget to destructure:
+    render(props, state){
+        return (
+                <div>
+
+            </div>
+        );
+    }
+
+
+}
+ 
