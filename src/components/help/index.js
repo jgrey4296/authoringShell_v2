37e25c0cc2542f1a93017cb25a4219486f9b8b23
John Grey <jgrey@ucsc.edu>
2017-03-21 23:06:30 +0000

    added a stubhelp component

---
+++ src/components/help/index.js
@@ -0,0 +1,33 @@
+import { h, Component } from 'preact';
+import React from 'react'; //aliased, for proptypes
+import style from './style';
+
+/**
+   General help display
+ */
+
+export default class Help extends Component {
+    //snippet::proptypes
+
+    constructor(props){
+        super(props);
+        //modify with this.setState
+        this.state = {
+            
+        };
+
+        //if necessary, bind methods
+        //this.aFunc = this.aFunc.bind(this)
+    }
+
+    //Don't forget to destructure:
+    render(props, state){
+        return (
+            <div>
+            This is the help section.
+            </div>
+        );
+    }
+
+
+}
